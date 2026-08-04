ca0ee5c5d4bd56de2238b35c78e83a73624596d1
diego3893 <diegozcx@foxmail.com>
2026-08-04 20:34:44 +0800

    docs: 添加每个常量的详细注释

--- include/fsa/config.hpp
+++ include/fsa/config.hpp
@@ -1,7 +1,7 @@
 /**
  * @file config.hpp
  * @author diego3893 (diegozcx@foxmail.com)
- * @brief FSA工程编译期参数定义，如数组大小、模块数量等
+ * @brief 定义FSA-HLS的编译期硬件规模参数
  * @date 2026-08-04
  * 
  * 
@@ -10,23 +10,37 @@
 #define CONFIG_HPP
 
 namespace fsa{
-
+/// @brief SA行数
 constexpr int SA_ROWS = 4;
-constexpr int SA_COLS = 4;
-constexpr int SPAD_ROWS = 2*SA_COLS+4*SA_ROWS; // spad存储深度，QKV*2
-constexpr int ACC_ROWS = 1+SA_ROWS; // 1行存L，rows行存O
 
-// 三个DMA参数，暂时不管
+/// @brief SA列数
+constexpr int SA_COLS = 4;
+
+/// @brief Scratchpad SRAM行数
+constexpr int SPAD_ROWS = 2*SA_COLS+4*SA_ROWS;
+
+/// @brief Accumulator SRAM行数
+constexpr int ACC_ROWS = 1 + SA_ROWS;
+
+/// @brief 外部内存访问端口数
 constexpr int nMemPorts = 4;
+
+/// @brief LoadQueue最多保存请求数
 constexpr int dmaLoadInflight = 16;
+
+/// @brief StoreQueue最多保存请求数
 constexpr int dmaStoreInflight = 8;
 
+/// @brief PWL区间数
 constexpr int exp2PwlPieces = 8;
 
-constexpr int N_SEMAPHORES = 32; // 信号数组大小
+/// @brief 信号量槽位数量
+constexpr int N_SEMAPHORES = 32;
 
-// TODO: 确定除法延迟
+/// @brief 倒数计算的延迟
 constexpr int reciprocalLatency = -1;
+// TODO: 延迟未确定
+
 }  // namespace fsa
 
 #endif // !CONFIG_HPP
