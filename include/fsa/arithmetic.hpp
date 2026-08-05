e0f66c8b65587a582c92bb52b7014386c993a90d
diego3893 <diegozcx@foxmail.com>
2026-08-06 01:57:40 +0800

    feat: 完善MAC计算单元

--- include/fsa/arithmetic.hpp
+++ include/fsa/arithmetic.hpp
@@ -11,7 +11,7 @@
 
 #include "fsa/types.hpp"
 
-namespace fsa {
+namespace fsa{
 
 /**
  * @brief PE的mac计算
@@ -22,6 +22,29 @@ namespace fsa {
  * @return acc_t a*b+c
  */
 acc_t peMac(elem_t in_a, elem_t in_b, acc_t in_c);
+
+/// @brief PE计算单元输出
+struct PeMacUnitOutput{
+    /// @brief acc_t结果，上下方向使用
+    acc_t out_accType{};
+
+    /// @brief elem_t结果，左右方向使用
+    elem_t out_elemType{};
+
+    /// @brief PWL分段是否与x匹配
+    bool out_exp2 = false;
+};
+
+/**
+ * @brief 执行 PE 内部 MacUnit 的普通 MAC 或 exp2 模式
+ *
+ * @param in_a PE.reg
+ * @param in_b l_input or slope
+ * @param in_c u/d_input or intercept
+ * @param in_exp2 true则exp2，否则MAC
+ * @return PeMacUnitOutput 两种精度的计算结果与exp2标志
+ */
+PeMacUnitOutput peMacUnit(elem_t in_a, elem_t in_b, acc_t in_c, bool in_exp2);
 
 /**
  * @brief Acc的mac计算
@@ -81,6 +104,13 @@ elem_t viewAasE(acc_t a);
  * @return elem_t PWL结果
  */
 elem_t peExp2Pwl(elem_t x, elem_t slope, acc_t intercept);
+
+/**
+ * @brief 取得CMP当前需要发送的PWL截距
+ * @param index exp2_counter的当前值
+ * @return acc_t 当前分段的截距
+ */
+acc_t exp2PwlIntercept(exp2_counter_t index);
 
 /**
  * @brief Acc的exp2计算
