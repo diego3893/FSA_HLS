adbd334d18901264e872d3614ee7d48707723599
diego3893 <diegozcx@foxmail.com>
2026-08-10 15:48:19 +0800

    fix: 删除转义

--- hls/pe/run_hls.tcl
+++ hls/pe/run_hls.tcl
@@ -11,7 +11,7 @@ open_project -reset $HLS_PROJECT_DIR
 
 set_top pe_top
 
-set CFLAGS "-std=c++14 -I\"[file join $PROJECT_ROOT include]\""
+set CFLAGS "-std=c++14 -I[file join $PROJECT_ROOT include]"
 
 add_files [file join $PROJECT_ROOT "src/hls/pe_top.cpp"]       -cflags $CFLAGS
 add_files [file join $PROJECT_ROOT "src/core/pe.cpp"]          -cflags $CFLAGS
