656f7067292e7d383daa7b8cadc4de08c1c7bb78
diego3893 <diegozcx@foxmail.com>
2026-08-11 13:32:09 +0800

    feat: 实现SA顶层模块

---
+++ src/hls/systolic_array_top.cpp
@@ -0,0 +1,53 @@
+#include "fsa/hls/systolic_array_top.hpp"
+#include "fsa/systolic_array.hpp"
+
+void systolic_array_top(const fsa::SystolicArrayInput& input,
+        fsa::SystolicArrayOutput& output){
+    #pragma HLS INTERFACE ap_ctrl_hs port=return
+    #pragma HLS AGGREGATE variable=input compact=bit
+    #pragma HLS AGGREGATE variable=output compact=bit
+    #pragma HLS INTERFACE ap_none port=input
+    #pragma HLS INTERFACE ap_none port=output
+
+    static fsa::SystolicArrayState current{};
+    
+    #pragma HLS ARRAY_PARTITION variable=current.mesh type=complete dim=0
+    #pragma HLS ARRAY_PARTITION variable=current.cmp_array type=complete dim=1
+    #pragma HLS ARRAY_PARTITION variable=current.cmp_ctrl_pipe type=complete dim=1
+    #pragma HLS ARRAY_PARTITION variable=current.pe_ctrl_pipe type=complete dim=0
+    #pragma HLS ARRAY_PARTITION variable=current.cmp_d_output_pipe type=complete dim=1
+    #pragma HLS ARRAY_PARTITION variable=current.r_output_pipe type=complete dim=0
+    #pragma HLS ARRAY_PARTITION variable=current.d_output_pipe type=complete dim=0
+    #pragma HLS ARRAY_PARTITION variable=current.u_output_pipe type=complete dim=0
+
+    if(input.reset){
+        fsa::reset_systolic_array_state(current);
+        output = fsa::SystolicArrayOutput{};
+        return;
+    }
+
+    fsa::SystolicArrayIO io{};
+    io.cmp_ctrl = input.cmp_ctrl;
+    io.pe_data = input.pe_data;
+
+    #pragma HLS ARRAY_PARTITION variable=io.pe_ctrl type=complete dim=1
+    #pragma HLS ARRAY_PARTITION variable=io.pe_data type=complete dim=1
+    #pragma HLS ARRAY_PARTITION variable=io.acc_out type=complete dim=1
+
+    for(int row=0; row<fsa::SA_ROWS; ++row){
+        #pragma HLS UNROLL
+        io.pe_ctrl[row] = input.pe_ctrl[row];
+    }
+
+    fsa::SystolicArrayState next{};
+    fsa::systolic_array_step(current, next, io);
+
+    for(int col=0; col<fsa::SA_COLS; ++col){
+        #pragma HLS UNROLL
+        output.acc_out[col] = io.acc_out[col];
+    }
+
+    current = next;
+
+    return;
+}
