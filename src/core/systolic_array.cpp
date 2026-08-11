41c9abfd299a6c9ad96a94168f7de43d530dcb61
diego3893 <diegozcx@foxmail.com>
2026-08-11 13:30:18 +0800

    feat: 展开所有循环

--- src/core/systolic_array.cpp
+++ src/core/systolic_array.cpp
@@ -1,4 +1,3 @@
-// TODO: #pragma 修饰是必须的！
 #include "fsa/systolic_array.hpp"
 #include "fsa/arithmetic.hpp"
 #include "fsa/cmp.hpp"
@@ -7,8 +6,11 @@
 namespace fsa{
 
     void reset_systolic_array_state(SystolicArrayState& state){
+        #pragma HLS INLINE
         for(int row=0; row<SA_ROWS; ++row){
+            #pragma HLS UNROLL
             for(int col=0; col<SA_COLS; ++col){
+                #pragma HLS UNROLL
                 reset_pe_state(state.mesh[row][col]);
                 state.pe_ctrl_pipe[row][col] = ValidData<PECtrl>{};
                 state.r_output_pipe[row][col] = ValidData<elem_t>{};
@@ -17,6 +19,7 @@ namespace fsa{
             }
         }
         for(int col=0; col<SA_COLS; ++col){
+            #pragma HLS UNROLL
             reset_cmp_state(state.cmp_array[col]);
             state.cmp_ctrl_pipe[col] = ValidData<CmpControl>{};
             state.cmp_d_output_pipe[col] = ValidData<acc_t>{};
@@ -26,13 +29,16 @@ namespace fsa{
 
     void systolic_array_step(const SystolicArrayState& current,
                             SystolicArrayState& next, SystolicArrayIO& io){
+        #pragma HLS INLINE
         next = current;
 
         for(int col=0; col<SA_COLS; ++col){
+            #pragma HLS UNROLL
             io.acc_out[col] = current.d_output_pipe[SA_ROWS-1][col];
         }
 
         for(int col=0; col<SA_COLS; ++col){
+            #pragma HLS UNROLL
             CMPIO cmp_io{};
             if(col == 0){
                 cmp_io.in_ctrl = io.cmp_ctrl;
@@ -48,7 +54,9 @@ namespace fsa{
         }
 
         for(int row=0; row<SA_ROWS; ++row){
+            #pragma HLS UNROLL
             for(int col=0; col<SA_COLS; ++col){
+                #pragma HLS UNROLL
                 PEIO pe_io{};
 
                 if(col == 0){
