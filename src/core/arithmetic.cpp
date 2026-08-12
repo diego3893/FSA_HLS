9e5ed1231c6ec3aa4e00df5d4e568557231653a2
diego3893 <diegozcx@foxmail.com>
2026-08-12 10:37:32 +0800

    feat: 手动添加latency，尝试切断组合链

--- src/core/arithmetic.cpp
+++ src/core/arithmetic.cpp
@@ -287,6 +287,7 @@ namespace fsa{
     elem_t cvtAtoE(const acc_t a){
         #pragma HLS INLINE off
         #pragma HLS PIPELINE II=1
+        #pragma HLS LATENCY min=1 max=1
         return (elem_t)a;
     }
 
