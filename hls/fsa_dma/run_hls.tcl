eb1a21eab7fa48b0ecea2dbf899b1333b0b9e276
diego3893 <diegozcx@foxmail.com>
2026-08-24 11:27:57 +0800

    fix: 暂时取消IP导出

--- hls/fsa_dma/run_hls.tcl
+++ hls/fsa_dma/run_hls.tcl
@@ -1,6 +1,6 @@
 set RUN_CSIM  1
 set RUN_COSIM 1
-set EXPORT_IP 1
+set EXPORT_IP 0
 
 set SCRIPT_DIR [file dirname [file normalize [info script]]]
 set PROJECT_ROOT [file normalize [file join $SCRIPT_DIR "../.."]]
@@ -9,7 +9,23 @@ set HLS_PROJECT_DIR [file join $SCRIPT_DIR "build"]
 open_project -reset $HLS_PROJECT_DIR
 set_top fsa_dma_top
 
-set CFLAGS "-std=c++14 -I[file join $PROJECT_ROOT include]"
+set SA_ROWS 4
+set SA_COLS 4
+set MAX_SEQUENCE_LENGTH 4096
+if {[info exists ::env(FSA_SA_ROWS)]} {
+    set SA_ROWS $::env(FSA_SA_ROWS)
+}
+if {[info exists ::env(FSA_SA_COLS)]} {
+    set SA_COLS $::env(FSA_SA_COLS)
+}
+if {[info exists ::env(FSA_MAX_SEQUENCE_LENGTH)]} {
+    set MAX_SEQUENCE_LENGTH $::env(FSA_MAX_SEQUENCE_LENGTH)
+}
+
+puts "FSA configuration: SA_ROWS=$SA_ROWS SA_COLS=$SA_COLS L_MAX=$MAX_SEQUENCE_LENGTH"
+set QKV_DEPTH [expr {$MAX_SEQUENCE_LENGTH*$SA_ROWS/4}]
+set O_DEPTH [expr {$MAX_SEQUENCE_LENGTH*$SA_ROWS/2}]
+set CFLAGS "-std=c++14 -I[file join $PROJECT_ROOT include] -DFSA_SA_ROWS=$SA_ROWS -DFSA_SA_COLS=$SA_COLS -DFSA_MAX_SEQUENCE_LENGTH=$MAX_SEQUENCE_LENGTH -DFSA_DMA_AXI_QKV_DEPTH=$QKV_DEPTH -DFSA_DMA_AXI_O_DEPTH=$O_DEPTH"
 
 add_files [file join $PROJECT_ROOT "src/hls/fsa_dma_top.cpp"] \
     -cflags $CFLAGS
